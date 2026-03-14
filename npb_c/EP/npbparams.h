d586d62778264abac08259c3fa39bafc5136da68
Nick Brown <nick@mesham.com>
2026-03-14 18:49:45 +0000

    C version of NPB

---
+++ npb_c/EP/npbparams.h
@@ -0,0 +1,18 @@
+/* CLASS = B */
+/*
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class of the NPB
+c  in this directory. Do not modify it by hand.
+*/
+#define	CLASS	 'B'
+#define	M	30
+#define	CONVERTDOUBLE	FALSE
+#define COMPILETIME "14 Mar 2026"
+#define NPBVERSION "3.0 structured"
+#define CS1 "gcc"
+#define CS2 "$(CC)"
+#define CS3 "-lm"
+#define CS4 "(none)"
+#define CS5 "-O3 -fopenmp -I../common"
+#define CS6 "$(CFLAGS)"
+#define CS7 "randdp"
